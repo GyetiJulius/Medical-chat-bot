909ea2cbefc643394ade7da9175948781dc1c1bb
Julius Gyeti 10989434 <jgyeti@st.ug.edu.gh>
2024-11-27 01:57:55 -1200

    Added the folder structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Medical-chat-bot
+
+### STEP 1: Create your virtual environment
+for windows: conda create -n medibot python==3.10 -y
+
+### STEP 2: Create your requirements.txt file
+requirements.txt
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from gettext import install
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical Chat bot',
+    packages=find_packages(),
+    version='0.1.0',
+    author='Julius Gyeti',
+    license='MIT',
+    install_requires = [],
+    author_email="gyetijulius5@gmail.com"
+)
